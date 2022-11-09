0aa96da2af2bfdfed67efc36df5da6f2736bc057
Ozlem Cali <ozlemmcali@gmail.com>
2022-11-09 14:17:13 +0300

    Initial Commit

---
+++ ToDoListCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoListCoreData
+//
+//  Created by ozlem on 9.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
