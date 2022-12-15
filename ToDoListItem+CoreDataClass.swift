854542e9821809cd5b410bab90994159257f20c9
Ozlem Cali <ozlemmcali@gmail.com>
2022-12-15 09:20:38 +0300

    first commit

---
+++ ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoListCoreData
+//
+//  Created by ozlem on 9.11.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
